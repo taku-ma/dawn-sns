adf96c57ca1c73d61e4d4da1a729869b5c997d94
takuma kusubaru <takuma@takuMBA.local>
2024-03-11 08:23:01 +0900

    create users_table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,9 +17,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('bio')->nullable();
+            $table->string('image')->nullable()->default('public/images/icons/dawn.png');
+            $table->rememberToken()->nullable();
             $table->timestamps();
         });
     }
