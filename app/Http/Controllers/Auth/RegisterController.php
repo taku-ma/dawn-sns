0a3e9444ed3cce5b02f07f1e6ce1aca61a7c3a92
takuma kusubaru <takuma@takuMBA.local>
2024-03-10 19:06:41 +0900

    register エラーメッセージ追加

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -54,9 +54,35 @@ class RegisterController extends Controller
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users', 'regex:/^[\w\-.@]+$/'],
             'password' => ['required', 'string', 'between:8,128', 'regex:/^[a-zA-Z0-9!@#$%^&*()_+|<>?{}\-=[\];.,]+$/','confirmed'],
             'password_confirmation' => ['required'],
-        ]);
+        ], $this -> validationMessages());
     }
 
+    /**
+     * Get the validation messages that apply to the request.
+     *
+     * @return array
+     */
+    protected function validationMessages()
+    {
+        return [
+            'name.required' => 'ユーザー名を入力してください。',
+            'name.string' => 'ユーザー名は4文字以上12文字以内で入力してください。',
+            'name.between' => 'ユーザー名は4文字以上12文字以内で入力してください。',
+            'name.regex' => 'ユーザー名には全角漢字カタカナひらがな数字記号もしくは半角英数記号を入力してください。',
+            'email.required' => 'メールアドレスを入力してください。',
+            'email.string' => 'メールアドレスは255文字以内で入力してください。',
+            'email.email' => 'メールアドレスは正しい形式で入力してください。',
+            'email.max' => 'メールアドレスは255文字以内で入力してください。',
+            'email.unique' => '入力されたメールアドレスはすでに登録されています。別のメールアドレスを登録してください。',
+            'email.regex' => 'メールアドレスには利用可能な文字を入力してください。',
+            'password.required' => 'パスワードを入力してください。',
+            'password.string' => 'パスワードは8文字以上128文字以内で入力してください。',
+            'password.between' => 'パスワードは8文字以上128文字以内で入力してください。',
+            'password.regex' => 'パスワードには半角英数記号を入力してください。',
+            'password.confirmed' => 'パスワードと確認の入力が一致しません。',
+            'password_confirmation.required' => 'パスワード確認を入力してください。',
+        ];
+    }
     /**
      * Create a new user instance after a valid registration.
      *
